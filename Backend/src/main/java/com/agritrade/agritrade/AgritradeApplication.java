5dba24d55da7a49ecd997950abc429866577e3d2
AJEET KUMAR <you@example.com>
2026-06-17 09:41:42 +0530

    Update backend logic for Agritrade

--- Backend/src/main/java/com/agritrade/agritrade/AgritradeApplication.java
+++ Backend/src/main/java/com/agritrade/agritrade/AgritradeApplication.java
@@ -1,13 +1,33 @@
 package com.agritrade.agritrade;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication
+@Slf4j
 public class AgritradeApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+
+		String yearMonth = new SimpleDateFormat("yyyy_MMMM").format(new Date());
+		String logFolder = "AppLogs/"+yearMonth;
+		Files.createDirectories(Paths.get(logFolder));
+
+		String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+		String logfileName = logFolder + "/"+ date + ".log";
+		System.setProperty("logging.file.name",logfileName);
+
+
 		SpringApplication.run(AgritradeApplication.class, args);
+		log.info("Agritrade Application Started Successfully");
+//		log.info("Log File Location : {}", logfileName);
 	}
 
 }
--- Backend/src/main/java/com/agritrade/agritrade/entity/PurchaseGrain.java
+++ Backend/src/main/java/com/agritrade/agritrade/entity/PurchaseGrain.java
@@ -5,14 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name="purchase_grain")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Entity
+//@Table(name="purchase_grain")
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class PurchaseGrain {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long purchaseId;
-    private Enu
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long purchaseId;
+
 }
